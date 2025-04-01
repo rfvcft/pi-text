c48fbb82d71cf746eca9e28a1417a3436ce33e2c
Nils Thorin <thorin.nils@gmail.com>
2025-04-01 12:06:43 +0200

    added possibility to make arbitrary stream instead of pi

--- pi_text.py
+++ pi_text.py
@@ -2,26 +2,50 @@ import pyfiglet
 from mpmath import mp
 
 
-def pi_text(text, symbol="#"):
+def pi_text(text, fill=True, custom_stream=None):
+    """
+    Create ascii art of the given text `text`.
+    If `fill` is True, have a background to the text which creates a "3D-effect"
+    If `custom_stream` is given then this string (repeated) will be used to fill the text in the ascii art
+    If `custom_stream` is not given (being None), the digits of pi is used as default
+    """
+
+    symbol = "#"
+    fill_symbs = [":", ".", "'"]
+
     ascii_text = pyfiglet.figlet_format(text, font="banner3-D")
 
     d = ascii_text.count(symbol)
-    mp.dps = d-1
-    pi = str(mp.pi)
+
+    if custom_stream is None:
+        # If no custom stream is defined, let the stream be the right amount of digits of pi
+        mp.dps = d-1
+        stream = str(mp.pi)
+    else:
+        # Otherwise, repeat the custom stream until it is longer than the required length
+        stream = custom_stream
+        while len(stream) < d:
+            stream += custom_stream
 
     digit_count = 0
-    pi_text = ""
-    for i, char in enumerate(ascii_text):
+    out_text = ""
+    for char in ascii_text:
         if char == symbol:
-            pi_text += pi[digit_count]
+            out_text += stream[digit_count]
             digit_count += 1
+
+        elif not fill and char in fill_symbs:
+            out_text += " "
+
         else:
-            pi_text += char
+            out_text += char
 
     header = "\n"*5 + "="*60 + "\n"*5
-    
-    return header + pi_text + header
+
+    return header + out_text + header
 
 if __name__ == '__main__':
     text = "GRATTIS LOVISA!"
-    print(pi_text(text))
+    stream = "peepeepoopoo"
+    print(pi_text(text, fill=False, custom_stream=stream))
+
